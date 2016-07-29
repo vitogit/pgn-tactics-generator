7e875c1c2f5a5f9312ecd250a99d7e8c174d3dbd
clarkerubber <jamesjosephclarke@gmail.com>
2016-07-30 04:36:18 +1000

    restructure modules

---
+++ modules/puzzle/analysed.py
@@ -0,0 +1,18 @@
+class analysed:
+    def __init__(self, move, evaluation):
+        self.move = move
+        self.evaluation = evaluation
+
+    def sign(self, val):
+        if val <= 0:
+            return -1
+        else:
+            return 1
+
+    def sort_val(self):
+        if self.evaluation.cp is not None:
+            return self.evaluation.cp
+        elif self.evaluation.mate is not None:
+            return self.sign(self.evaluation.mate) * (abs(self.evaluation.mate) + 1) * 10000
+        else:
+            return 0
