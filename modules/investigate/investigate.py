1fc4aac5b2195d3f491babf17e6e0b6dc91f63af
clarkerubber <jamesjosephclarke@gmail.com>
2016-08-02 10:14:30 +1000

    debug and more allowance for losing on last_pos

--- modules/investigate/investigate.py
+++ modules/investigate/investigate.py
@@ -1,5 +1,13 @@
 import chess
 
+def sign(a):
+    if a > 0:
+        return 1
+    elif a < 0:
+        return -1
+    else:
+        return 0
+
 def material_value(board):
     total = 0
     for i in chess.SQUARES:
@@ -19,14 +27,19 @@ def investigate(a, b, board):
     # determine if the difference between position A and B 
     # is worth investigating for a puzzle.
     if a.cp is not None and b.cp is not None:
-        if (((a.cp < 850 and a.cp > -110 and b.cp < -200 and b.cp > -850)
-            or (a.cp > -850 and a.cp < 110 and b.cp > 200 and b.cp < 850))
-            and material_value(board) > 3):
+        if (((a.cp < 850 and a.cp > -110 and b.cp > 200 and b.cp < 850)
+            or (a.cp > -850 and a.cp < 110 and b.cp < -200 and b.cp > -850))
+            and material_value(board) > 3
+            and sign(a.cp) == sign(b.cp)):
             return True
     elif (a.cp is not None
         and b.mate is not None
         and material_value(board) > 3):
-        if abs(a.cp) < 200:
+        if ((a.cp < 110 and sign(b.mate) == -1)
+            or (a.cp > -110 and sign(b.mate) == 1)):
             return True
-    else:
-        return False
+    elif (a.mate is not None
+        and b.mate is not None):
+        if sign(a.mate) == sign(b.mate): #actually means that they're opposite
+            return True
+    return False
